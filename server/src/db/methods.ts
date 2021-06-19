675929460c77f1bae8e8e0fc7a47ec5a0b7f34ea
Dvir Bartov <dvirbartov@Dvirs-MacBook-Pro.local>
2021-06-20 02:42:48 +0300

    #9 Add enhanced model interface and update method

--- server/src/db/methods.ts
+++ server/src/db/methods.ts
@@ -1,6 +1,6 @@
 import { Document, Model, Schema } from "mongoose";
 
-export async function getByIdOrFail(this: Model<Schema>, id: string): Promise<Document> {
+export async function getByIdOrFail(this: Model<Schema>, id: string): Promise<Document<Schema>> {
     const doc = await this.findById(id);
 
     if(!doc) {
--- server/src/db/types.d.ts
+++ server/src/db/types.d.ts
@@ -1,7 +1,13 @@
+import { Model } from "mongoose";
+
 type StaticMethod = (...any) => any;
 
 export interface StaticMethodsOptions {
     customStaticMethods?: {
         [key: string]: StaticMethod
     }
+}
+
+export interface EnhancedModel<T> extends Model<T> {
+    getByIdOrFail: (this: Model<Schema>, id: string) => Promise<Document<T>>
 }
